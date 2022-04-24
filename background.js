e9531c80fb0ddb518aa58927b1d0c595c96dadb8
Brendan Doyle <brendandoyle060@gmail.com>
2022-04-24 13:15:01 -0400

    Fixed a lot of formatting to make it more consistent and clean

--- background.js
+++ background.js
@@ -1,55 +1,61 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function inject(tab) {
+chrome.browserAction.onClicked.addListener(
+    function inject(tab) {
 
-    // console.log("browserAction onClicked");
+        // console.log("browserAction onClicked");
 
-    // Break if we're not on an ESPN Fantasy Baseball My Team page
-    if ( !isValidPage(tab.url) ){
-        // console.log("if !isValidPage");
-        return;
-    }
-
-    // console.log("created inject listener");
-
-    // var myCode = "console.log('Testing...this code will execute as a content script');";
-    chrome.tabs.executeScript(
-        null,
-        {
-            file: "content.js"
-        }, 
-        function(){
-            if(chrome.runtime.lastError) {
-                console.error("Script injection failed: " + chrome.runtime.lastError.message);
-            }
+        // Break if we're not on an ESPN Fantasy Baseball My Team page
+        if (!isValidPage(tab.url)) {
+            // console.log("if !isValidPage");
+            return;
         }
-    );
-    // console.log("chrome.tabs.executeScript content.js");
 
-    // Send a message to the active tab
-    chrome.tabs.query({
-        active: true, 
-        lastFocusedWindow: true
-    }, 
-    function(tabs) {
-        // console.log("chrome.tabs.query");
-        // console.log("tabs.length: " + tabs.length);
+        // console.log("created inject listener");
 
-        var activeTab = tabs[0];
-        var index = activeTab.index;
-        chrome.tabs.sendMessage(activeTab.id, {
-            "message": "clicked_browser_action", 
-            "url": tab.url,
-            "index": index + 1
-        });
+        // var myCode = "console.log('Testing...this code will execute as a content script');";
+        chrome.tabs.executeScript(
+            null,
+            {
+                file: "content.js"
+            },
+            function () {
+                if (chrome.runtime.lastError) {
+                    console.error("Script injection failed: " + chrome.runtime.lastError.message);
+                }
+            }
+        );
+        // console.log("chrome.tabs.executeScript content.js");
 
-            // console.log("sendMessage - tab url: " + tab.url);
-            // console.log("end chrome.tabs.query");
+        // Send a message to the active tab
+        chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true
+        },
+            function (tabs) {
+                // console.log("chrome.tabs.query");
+                // console.log("tabs.length: " + tabs.length);
+
+                var activeTab = tabs[0];
+                var index = activeTab.index;
+                chrome.tabs.sendMessage(activeTab.id, {
+                    "message": "clicked_browser_action",
+                    "url": tab.url,
+                    "index": index + 1
+                });
+
+                // console.log("sendMessage - tab url: " + tab.url);
+                // console.log("end chrome.tabs.query");
+            });
+
     });
 
-});
-
+/**
+ * 
+ * @param {String} url 
+ * @returns {boolean} true if the given url is valid
+ */
 function isValidPage(url) {
     // console.log("isValidPage? url: " + url);
 
@@ -60,7 +66,7 @@ function isValidPage(url) {
 chrome.runtime.onMessage.addListener(
     function openNewTab(request, sender, sendResponse) {
         // console.log("chrome.runtime.onMessage.addListener - open_new_tab");
-        if( request.message === "open_new_tab" ) {
+        if (request.message === "open_new_tab") {
             chrome.tabs.create({
                 url: request.url,
                 index: request.index
