d5b0c2f33c3152905f5d7924513258d10d63c9bd
Brendan Doyle <brendandoyle060@gmail.com>
2022-04-24 13:14:04 -0400

    WIP: started creation of basic HTML elements that will be added into the popup based on pitcher information pulled from the page

--- popup.js
+++ popup.js
@@ -1,20 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('firstStarter');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'https://fantasy.espn.com/';
-        f.method = 'get';
-        var i = d.createElement('input');
-        // i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
+document.addEventListener('DOMContentLoaded', function () {
+    var thisWeekTable = document.getElementById('firstStarter');
+    thisWeekTable.addEventListener('click', function () {
+
+        chrome.tabs.getSelected(function (tab) {
+            d = document;
+            var tr = d.createElement('tr');
+
+            var tdName = d.createElement('td');
+            tdName.setAttribute("name", "name");
+            tdName.innerHTML = 'tdName example';
+            var tdTeam = d.createElement('td');
+            tdTeam.setAttribute("name", "team");
+            tdTeam.innerHTML = 'tdTeam example';
+            var tdOpponent = d.createElement('td');
+            tdOpponent.setAttribute("name", "opponent");
+            tdOpponent.innerHTML = 'tdOpponent example';
+            var tdDate = d.createElement('td');
+            tdDate.setAttribute("name", "date");
+            tdDate.innerHTML = 'tdDate example';
+            var tdToggle = d.createElement('td');
+            tdToggle.setAttribute("name", "toggle");
+            tdToggle.innerHTML = 'tdToggle example';
+
+            tr.appendChild(tdName);
+            tr.appendChild(tdTeam);
+            tr.appendChild(tdOpponent);
+            tr.appendChild(tdDate);
+            tr.appendChild(tdToggle);
+
+            table = d.getElementById("thisWeek");
+            table.appendChild(tr);
+
+        });
     }, false);
-  }, false);
+}, false);
+
+function createStartRow(tableId) {
+    // name, team, opponent, date, toggle // photo?
+    var tr = document.createElement('tr');
+
+    var tdName = document.createElement('td');
+    var tdTeam = document.createElement('td');
+    var tdOpponent = document.createElement('td');
+    var tdDate = document.createElement('td');
+    var tdToggle = document.createElement('td');
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdTeam);
+    tr.appendChild(tdOpponent);
+    tr.appendChild(tdDate);
+    tr.appendChild(tdToggle);
+
+}
